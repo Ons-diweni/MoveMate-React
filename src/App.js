19c0430bfdef15ee829aec08124ce7c7e68e6df0
Ons <ons.diweni@esprit.tn>
2023-03-16 07:59:06 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Navbar} from './components/navbar';
+
+
+//le composant racine de l'application 
+function App() {
+  return (
+    <div >
+      
+      <Navbar/>
+     
+    </div>
+  );
+}
+
+export default App;
